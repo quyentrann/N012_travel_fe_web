6b85c5ff22692541a689a442dacbdab74ba6db7f
quyentrann <quyentrann2k2@gmail.com>
2025-05-20 14:56:42 +0700

    fix change tour

--- src/components/ItemTourBookingDetail.jsx
+++ src/components/ItemTourBookingDetail.jsx
@@ -93,6 +93,7 @@ export default function TourBookingForm({
           departureDate: startDate,
           notes,
         },
+
         {
           headers: {
             Authorization: `Bearer ${token}`,
